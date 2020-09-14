362bf4b5f096b56f78505fa8de3f70a2a79bd688
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-09-14 19:50:25 +0900

    영속성관리 - SQL 실행 시점

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,17 +12,14 @@ public class JpaMain {
 
         try{
 
-            //비영속 상태
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloB");
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            //영속 상태
-            System.out.println(" === BEFORE ===");
-            em.persist(member);
-            System.out.println(" === AFTER ===");
-            //여기까진 영속 상태로 변경만되고, DB에 저장되지는 않음
+            //영속성 컨텍스트에 쌓아두고,
+            em.persist(member1);
+            em.persist(member2);
 
+            //커밋 시점에 SQL문이 실행된다.
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
