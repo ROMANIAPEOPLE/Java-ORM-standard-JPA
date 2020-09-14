3079a1003eee8d178d90091ebbcef8d80a8888ec
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-09-14 19:35:49 +0900

    영속성관리 - SQL 실행 시점

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,11 +11,18 @@ public class JpaMain {
         tx.begin();
 
         try{
+
+            //비영속 상태
             Member member = new Member();
-            member.setId(1L);
+            member.setId(100L);
             member.setName("HelloB");
 
+            //영속 상태
+            System.out.println(" === BEFORE ===");
             em.persist(member);
+            System.out.println(" === AFTER ===");
+            //여기까진 영속 상태로 변경만되고, DB에 저장되지는 않음
+
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -13,4 +13,14 @@ public class Member {
     @Id
     private Long id;
     private String name;
+
+
+    public Member(){
+
+    }
+
+    public Member(Long id , String name) {
+        this.id = id;
+        this.name=name;
+    }
 }
