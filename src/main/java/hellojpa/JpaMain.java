072207ab9502af7baaad477d5f293be9adbc2c2a
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-09-26 18:55:50 +0900

    지연로딩과 즉시로딩 정리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,18 @@ package hellojpa;
 import javax.persistence.*;
 import java.util.List;
 
+/** 지연로딩과 즉시로딩
+ *Fetch = FetchType.LAZY 하면 , LAZY로 설정된 것은 모두 프록시 객체로 조회한다.
+ * 또한 , OneToOne과 ManyToOne만 Default 값이 EAGER고, 나머지는 모두 LAZY로 설정되어있다.
+ * 실제로 객체를 사용하는 시점에 프록시가 초기화되면서 쿼리가 실행된다.
+ *
+ * 예를들어 Member를 조회할때 Team이 ManyToOne 관계로 외래키로 잡혀있다면, Member를 조회하는 동시에
+ * 자동으로 Team도 조회가 된다.
+ * 이 때, Team의 FetchType을 Lazy로 설정해준다면 Member를 조회할때 Team이 조회되지 않는다.
+ * 결론적으로, 실무에서 EAGER(즉시로딩)은 99프로 사용하지 말아야한다.
+ *
+ */
+
 public class JpaMain {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
